4be3d62afd3450c657c88a23862ea44cf8deaa82
mgualteros <mgualterosg@ucentral.edu.co>
2025-04-19 01:17:34 -0500

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,16 @@
 def evaluate_model(model, x_test, y_test):
+    """
+Evalúa un modelo de Keras.
+
+Parámetros:
+- model (keras.Model): El modelo de Keras que se va a evaluar.
+- x_test (array): Datos de entrada para la evaluación.
+- y_test (array): Etiquetas de los datos de evaluación.
+
+Retorna:
+- loss (float): La pérdida calculada en los datos de prueba.
+- accuracy (float): La precisión calculada en los datos de prueba.
+"""
     loss, accuracy = model.evaluate(x_test, y_test)
     print(f"Loss: {loss:.4f} - Accuracy: {accuracy:.4f}")
     return loss, accuracy
